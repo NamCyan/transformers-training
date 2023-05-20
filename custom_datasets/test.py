fe882838278c24e4851fa59d4727b7afd9a00462
NamCyan <nkimxte@gmail.com>
2023-05-20 20:23:58 +0700

    first commit

--- custom_datasets/test.py
+++
@@ -1,4 +0,0 @@
-import datasets
-
-
-data = datasets.load_dataset("/home/namlh31/project/AI4Code/TheVault_exp/custom_datasets/human-eval.py", datapath= "/home/namlh31/project/AI4Code/testgen/data/human-eval/HumanEval.jsonl")
